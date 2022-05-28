debf0a41a564f563073bac9f2d0ccfd74b900da7
Rafael Lima <rafauxdev@gmail.com>
2022-05-28 04:42:54 +0100

    Add Section and usePlanets to the App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import style from './App.module.scss';
 import Home from './pages/Home';
 import Hero from './components/Hero';
+import Section from './components/Section';
+import usePlanets from './customHooks/usePlanets';
 
-const App = () => (
-  <div className={style.main}>
-    <Hero />
-    <Home />
-  </div>
-);
+const App = () => {
+  const { planets, reqStatus } = usePlanets();
+
+  return (
+    <div className={style.main}>
+      <Hero />
+      <main>
+        <Home />
+        <Section items={planets} reqStatus={reqStatus} />
+      </main>
+    </div>
+  );
+};
 
 export default App;
